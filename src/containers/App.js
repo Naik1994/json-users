2d0d37f3cebcb720ac4e85775baa47544dd0ec35
Naik1994 <harishsnaik94@gmail.com>
2024-03-03 13:18:40 +0530

    using react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // import Robots from './robots';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
@@ -59,34 +59,43 @@ import ErrorBoundry from '../components/ErrorBoundry';
 //   );
 // }
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: ""
-    };
-  }
+function App()  {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchField: ""
+  //   };
+  // }
 
-  componentDidMount() {
+  const [robots, setRobots ] = useState([]);
+  const [searchField, setSearchField ] = useState("");
+
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then(resp => resp.json())
+  //     .then(users => this.setState({robots: users}))
+  // }
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(resp => resp.json())
-      .then(users => this.setState({robots: users}))
+      .then(users => setRobots(users))
+  }, []);
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
-  }
-
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-    if (!this.state.robots.length) {
+  // render() {
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
+    if (!robots.length) {
       return <h1>Loading..</h1>
     } else {
       return (
         <div className="tc">
           <h1 className='f2'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundry>
               <CardList robots={filteredRobots}/>
@@ -96,7 +105,7 @@ class App extends Component {
       );      
     }
     
-  }
+  // }
 }
 
 export default App;
